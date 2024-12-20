c45700d3ca7ab0f3673baf184af1b5b1163eae9e
Zoran Jovanovic <zojovano@microsoft.com>
2024-12-20 11:26:58 +0100

    update

--- app/dotnet-client/OTELWorker/Program.cs
+++ app/dotnet-client/OTELWorker/Program.cs
@@ -1,3 +1,11 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Reflection.PortableExecutable;
+
 namespace OTELWorker;
 
 public class Program
@@ -6,6 +14,17 @@ public class Program
     {
         var builder = Host.CreateApplicationBuilder(args);
         builder.Services.AddHostedService<Worker>();
+
+        builder.Logging.ClearProviders();
+
+        // Initialize OpenTelemetry
+        builder.Services.AddOpenTelemetry().ConfigureResource((resourceBuilder) =>
+        {
+        resourceBuilder.AddService(
+            serviceName: builder.Configuration.GetValue("ServiceName", defaultValue: "otel-test")!,
+            serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
+            serviceInstanceId: Environment.MachineName);
+        });
 
         var host = builder.Build();
         host.Run();
--- app/dotnet-client/OTELWorker/obj/Release/net9.0/OTELWorker.AssemblyInfo.cs
+++ app/dotnet-client/OTELWorker/obj/Release/net9.0/OTELWorker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("dotnet-OTELWorker-8a336398-f5c1-4259-9fb5-04230a583356")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("OTELWorker")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c37c61160e4c2610c6118667034faf4d7151d9e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("OTELWorker")]
